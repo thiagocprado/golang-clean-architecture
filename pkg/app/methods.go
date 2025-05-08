229206ee65aa1f427f438298fe521fae894f292d
Thiago <thiagoprado549@gmail.com>
2025-05-07 22:35:07 -0300

    Revert "teste"
    This reverts commit fbab19876a2538a535c07860092dda4223945cdb.

--- pkg/app/methods.go
+++ pkg/app/methods.go
@@ -6,6 +6,16 @@ import (
 	"strings"
 )
 
+func FindInArray(array []string, value string) bool {
+	for _, v := range array {
+		if v == value {
+			return true
+		}
+	}
+
+	return false
+}
+
 func FindInArrayInt(values string, value int) bool {
 	array := strings.Split(values, ",")
 

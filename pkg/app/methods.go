a536c27027ccc8dce6eb36bc8b64fc5e43b116e6
Thiago <thiagoprado549@gmail.com>
2025-05-07 22:37:10 -0300

    add roundFloatV2

--- pkg/app/methods.go
+++ pkg/app/methods.go
@@ -34,3 +34,8 @@ func RoundFloat(val float64, precision uint) float64 {
 	ratio := math.Pow(10, float64(precision))
 	return math.Round(val*ratio) / ratio
 }
+
+func RoundFloatv2(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
+}
